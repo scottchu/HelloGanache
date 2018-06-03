daf7afbeb0f5c503ac5f5eff9564f75b4ee0e309
Scott Chu <scottchu@icloud.com>
2018-06-03 16:13:02 -0400

    complete assign

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  networks: {
-    ganache: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*"
-    }
-  }
-}
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+};
